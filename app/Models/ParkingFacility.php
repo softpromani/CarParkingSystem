60f082a621d6066c3f389b511e1723fabd100e3d
softpromani <sharmahcool5@gmail.com>
2025-06-29 01:15:04 +0530

    bug

--- app/Models/ParkingFacility.php
+++ app/Models/ParkingFacility.php
@@ -1,16 +1,19 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ParkingFacility extends Model
 {
-    protected $guarded =[];
+    protected $guarded = [];
+    protected $appends = ['full_image'];
 
     public function parkings()
-{
-    return $this->belongsToMany(Parking::class, 'parking_facility_maps', 'parking_facility_id', 'parking_id');
-}
-
+    {
+        return $this->belongsToMany(Parking::class, 'parking_facility_maps', 'parking_facility_id', 'parking_id');
+    }
+    public function getFullIage()
+    {
+        return asset('storage' . $this->image);
+    }
 }
