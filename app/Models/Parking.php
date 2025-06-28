2243169b5bbce522f31863c3d96db74666057e43
softpromani <sharmahcool5@gmail.com>
2025-06-28 23:18:32 +0530

    full image

--- app/Models/Parking.php
+++ app/Models/Parking.php
@@ -1,19 +1,24 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Parking extends Model
 {
-    protected $guarded =[];
+    protected $guarded = [];
+    protected $appends = ['full_image'];
 
     public function facilities()
-{
-    return $this->belongsToMany(ParkingFacility::class, 'parking_facility_maps', 'parking_id', 'parking_facility_id');
-}
-public function slots(){
-    return $this->hasMany(ParkingSlot::class,'parking_id');
-}
+    {
+        return $this->belongsToMany(ParkingFacility::class, 'parking_facility_maps', 'parking_id', 'parking_facility_id');
+    }
+    public function slots()
+    {
+        return $this->hasMany(ParkingSlot::class, 'parking_id');
+    }
+    public function getFullImageAttribute()
+    {
+        return asset('storage/' . $this->image);
+    }
 
 }
