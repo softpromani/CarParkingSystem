21073cbc29b64a4f23207a2132143cecb82329d9
softpromani <sharmahcool5@gmail.com>
2025-06-28 14:01:01 +0530

    bug

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Vehicle extends Model
 {
     protected $fillable = ['registration_number', 'owner_id', 'driver_id', 'model_id', 'brand_id', 'color', 'tyre', 'type'];
-    protected $with=['brnad_model','brand'];
+    protected $with=['brand_model','brand'];
     public function brand(): BelongsTo
     {
         return $this->belongsTo(Brand::class);
