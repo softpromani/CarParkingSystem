e86fd16718176dc402bd3807700b2491344d8530
softpromani <sharmahcool5@gmail.com>
2025-06-29 00:21:42 +0530

    full image

--- app/Http/Controllers/api/vehicleOwner/ProfileController.php
+++ app/Http/Controllers/api/vehicleOwner/ProfileController.php
@@ -73,7 +73,7 @@ class ProfileController extends Controller
     public function wallet_history()
     {
         $data['wallet_amount']  = auth()->user()->wallet;
-        $data['wallet_history'] = WalletHistory::where('user_id', auth()->id());
+        $data['wallet_history'] = WalletHistory::where('user_id', auth()->id())->paginate(20);
         return response()->json(['status' => true, 'data' => $data, 'message' => 'wallet history']);
     }
 }
