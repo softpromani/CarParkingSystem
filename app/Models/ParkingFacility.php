0b52b38bfaca63f29aa56a41fc8a1980a1823c2c
softpromani <sharmahcool5@gmail.com>
2025-06-29 01:25:50 +0530

    bug

--- app/Models/ParkingFacility.php
+++ app/Models/ParkingFacility.php
@@ -12,7 +12,7 @@ class ParkingFacility extends Model
     {
         return $this->belongsToMany(Parking::class, 'parking_facility_maps', 'parking_facility_id', 'parking_id');
     }
-    public function getFullIageAttribute()
+    public function getFullImageAttribute()
     {
         return asset('storage' . $this->image);
     }
